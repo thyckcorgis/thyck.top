03afb9281b1507cb469a75d03d3567983bb28651
Annette Lau <annette1@ualberta.ca>
2021-02-06 00:16:02 -0700

    stupid thing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./components.css";
 import NavBar from "./NavBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      <h1>This is the home page</h1>
+      <div className="homeScreen">
+        <h1 className="title">This is the home page</h1>
+      </div>
     </div>
   );
 }
