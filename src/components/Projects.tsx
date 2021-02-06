c9de8cfb44a20b8a0ecadbdb1d20696320cb2bb5
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-06 05:45:02 +0000

    poop

---
+++ src/components/Projects.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>This is the Projects page</h1>
+    </div>
+  );
+}
---
+++ src/components/Team.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Team() {
+  return (
+    <div>
+      <h1>This is the team page</h1>
+    </div>
+  );
+}
