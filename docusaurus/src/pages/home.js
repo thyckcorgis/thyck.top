e82e32933a2a099e7b648dda04a4674ad5d56638
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-05-06 21:05:18 -0600

    add home.js

---
+++ docusaurus/src/pages/home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import styles from "./index.module.css";
+export default function () {
+  return (
+    <Layout
+      title="We are thyck"
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <p className="title">
+            HelloWo rld?
+            <br />
+            No.
+            <br />
+            Hello Thyck.
+          </p>
+          <p className="body">
+            Thyck Corgis are back and better than ever, this time, in the form
+            of a website. We make projects, we get t h y c k.
+            <br />
+            <br />
+            Check out our latest app!
+          </p>
+          <div>
+            <button className="button">Heart 2 Heart</button>
+          </div>
+        </div>
+        <div className="iPhoneBox">
+          <img className="iPhone" src={allImages.Iphone} />
+        </div>
+      </header>
+    </Layout>
+  );
+}
