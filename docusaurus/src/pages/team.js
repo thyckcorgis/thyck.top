4035ef2c79850c2aaa019d0b292a999ab3361560
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-05-06 21:05:30 -0600

    add layout to team page

--- docusaurus/src/pages/team.js
+++ docusaurus/src/pages/team.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../css/custom.css";
+import Layout from "@theme/Layout";
 
 export default function Team() {
   return (
-    <div>
+    <Layout
+      title="We are thyck"
+      description="Description will go into a meta tag in <head />"
+    >
       <p className="title">This is the team page</p>
       <p className="body">Charles</p>
-    </div>
+    </Layout>
   );
 }
