b677a29bd7550a7cc5fc49d8feffd904b14c86c9
Annette Lau <annette1@ualberta.ca>
2021-05-07 20:18:44 -0600

    overlay letters and add uwu

--- docusaurus/src/pages/team.js
+++ docusaurus/src/pages/team.js
@@ -9,30 +9,54 @@ const TeamList = [
   {
     title: "Charles",
     url: "/img/charles.png",
-    description: <>This is Charles</>,
+    description: <>This is Charles </>,
+    altDescription: (
+      <>
+        {String.fromCodePoint(0x1f449)}
+        {String.fromCodePoint(0x1f448)}
+      </>
+    ),
     Svg: require("../../static/img/c.svg").default,
   },
   {
     title: "Annette",
     url: "/img/annette.png",
     description: <>This is Annette</>,
+    altDescription: (
+      <>
+        {String.fromCodePoint(0x1f449)}
+        {String.fromCodePoint(0x1f448)}
+      </>
+    ),
     Svg: require("../../static/img/a.svg").default,
   },
   {
     title: "Will",
     url: "/img/will.png",
     description: <>This is Will</>,
+    altDescription: (
+      <>
+        {String.fromCodePoint(0x1f449)}
+        {String.fromCodePoint(0x1f448)}
+      </>
+    ),
     Svg: require("../../static/img/w.svg").default,
   },
   {
     title: "Kim Juyoung",
     url: "/img/juyoung.png",
     description: <>This is Kim Juyoung</>,
+    altDescription: (
+      <>
+        {String.fromCodePoint(0x1f449)}
+        {String.fromCodePoint(0x1f448)}
+      </>
+    ),
     Svg: require("../../static/img/k.svg").default,
   },
 ];
 
-function Team({ title, url, description, Svg }) {
+function Team({ title, url, description, altDescription, Svg }) {
   const [isShown, setIsShown] = useState(false);
   return (
     <div className={clsx("col col--3")}>
@@ -41,16 +65,28 @@ function Team({ title, url, description, Svg }) {
         onMouseEnter={() => setIsShown(true)}
         onMouseLeave={() => setIsShown(false)}
       >
-        <img className={styles.personImg} src={url} alt={title} />
-        {isShown && (
+        {isShown ? (
           <div>
-            <Svg className={styles.personImg} alt={title} />
+            <Svg className={styles.letterSvg} alt={title} />
+            <img className={styles.personImgHover} src={url} alt={title} />
+          </div>
+        ) : (
+          <div>
+            <img className={styles.personImg} src={url} alt={title} />{" "}
           </div>
         )}
       </div>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <div>
+          {isShown ? (
+            <p className={styles.emoji}>{altDescription}</p>
+          ) : (
+            <div>
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
