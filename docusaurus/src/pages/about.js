7decfd6b0285d6092312da0926b7188a6eb54750
Annette Lau <annette1@ualberta.ca>
2021-05-09 19:34:40 -0600

    revamp about page

---
+++ docusaurus/src/pages/about.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "../css/custom.css";
+import Layout from "@theme/Layout";
+import styles from "./team.module.css";
+
+export default function AboutPage() {
+  return (
+    <Layout
+      title="Our Thyck Team"
+      description="Meet the people behind Thyck Corgis."
+    >
+      <div className="container">
+        <p className="title slide-in-left"> Through Thyck and Thin</p>
+        <div
+          className="headerLine slide-in-left"
+          style={{ animationDelay: "0.5s" }}
+        />
+        <div className="text-box text--center">
+          <p className="body fadeIn" style={{ animationDelay: "1s" }}>
+            As a team of programmers, but first, friends, our team was not
+            formed without humble beginnings. Founded in October, 2020, Thyck
+            Corgis entered their very first programming competition. Little did
+            they know, their love for programming would grow into something
+            thycker. Join Thyck Corgis in our journey of making software
+            projects.
+          </p>
+        </div>
+        <div className="fadeIn" style={{ animationDelay: "1.5s" }}>
+          <img className="img-rectangle " src="/img/uaec.jpg" />
+        </div>
+      </div>
+    </Layout>
+  );
+}
