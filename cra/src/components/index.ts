fda93c1bb54d959d8d2e62433dbc6c8a432c2313
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-05-10 21:44:58 -0600

    delete cra stuff

--- cra/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Team } from "./Team";
-export { default as Projects } from "./Projects";
-export { default as NavBar } from "./NavBar";
