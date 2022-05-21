e7ad2c59debb72195932de598e397bd630474108
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-05-21 05:53:35 -0600

    Create dynamic link  for iPhone box (#37)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,59 @@ import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
 import styles from "./index.module.css";
+import Link from "@docusaurus/Link";
+import useIsBrowser from "@docusaurus/useIsBrowser";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const HomePageTitle = () => (
+  <p className="title text--left slide-in-left">
+    <span>Hell</span>
+    <span className="owo">oWo</span>
+    <span>rld?</span>
+    <br />
+    No.
+    <br />
+    Hello Thyck.
+  </p>
+);
+
+const StaticIphoneBox = () => (
+  <div className="iPhoneBox fadeIn" style={{ animationDelay: "1s" }}>
+    <img src="/img/phone_wapp.webp" className="bottom" />
+    <img src="/img/phone_h2h.webp" className="top" />
+  </div>
+);
+
+const DynamicIphoneBox = () => {
+  const [link, setLink] = useState<"h2h" | "wapp">("h2h");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLink((v) => (v === "h2h" ? "wapp" : "h2h"));
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="iPhoneBox fadeIn" style={{ animationDelay: "1s" }}>
+      <Link to={"/projects/" + link}>
+        <img src="/img/phone_wapp.webp" className="bottom" />
+        <img src="/img/phone_h2h.webp" className="top" />
+      </Link>
+    </div>
+  );
+};
+
+const IPhoneBox = () => {
+  const isBrowser = useIsBrowser();
+  return isBrowser ? <DynamicIphoneBox /> : <StaticIphoneBox />;
+};
 
 export default function () {
   return (
     <Layout description="Thyck Corgis' Official Website">
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <p className="title text--left slide-in-left">
-            <span>Hell</span>
-            <span className="owo">oWo</span>
-            <span>rld?</span>
-            <br />
-            No.
-            <br />
-            Hello Thyck.
-          </p>
+          <HomePageTitle />
           <div
             className="headerLine slide-in-left"
             style={{
@@ -24,24 +62,30 @@ export default function () {
               animationDelay: "0.25s",
             }}
           />
-          <p
-            className="body text--left fadeIn"
-            style={{ animationDelay: "1s" }}
-          >
-            Thyck Corgis are back and better than ever, this time, in the form
-            of a website. We make projects, we get
-            <p className="thyck-spacing">t h y c k.</p>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <p
+              className="body text--left fadeIn"
+              style={{ animationDelay: "1s" }}
+            >
+              Thyck Corgis are back and better than ever, this time, in the form
+              of a website. We make projects, we get
+            </p>
+            <p
+              className="body text--left fadeIn thyck-spacing"
+              style={{ animationDelay: "1s" }}
+            >
+              t h y c k.
+            </p>
             <br />
-            Check out our latest app!
-          </p>
-          <div>
-            <button className="button">Heart 2 Heart</button>
+            <div className="fadeIn" style={{ animationDelay: "1s" }}>
+              Check out our latest app!
+            </div>
+            <div className="fadeIn" style={{ animationDelay: "1.5s" }}>
+              <button className="button">Heart 2 Heart</button>
+            </div>
           </div>
         </div>
-        <div className="iPhoneBox fadeIn" style={{ animationDelay: "1s" }}>
-          <img src="/img/phone_wapp.webp" className="bottom" />
-          <img src="/img/phone_h2h.webp" className="top" />
-        </div>
+        <IPhoneBox />
       </header>
     </Layout>
   );
